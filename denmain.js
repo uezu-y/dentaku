fd57e43ab261b91906a2a4335dc59aaa97bc3588
EC2 Default User <ec2-user@ip-172-31-5-229.ap-northeast-1.compute.internal>
2022-10-05 06:03:38 +0000

    電卓アプリ

---
+++ denmain.js
@@ -0,0 +1,166 @@
+$(document).ready(function ($) {
+	let n1;　//1番目の数値
+	let n2;　//2番目の数値
+	let operator;　　//演算子が格納される
+	let minus;　//マイナス値から計算する時に使用する
+	let operator_count;　//演算子の連続入力防止
+	
+	n1 = null;
+	n2 = null;
+	operator = null;
+	minus = false;
+	operator_count = 0;
+	
+	
+	//数値と演算子の入力を処理する関数
+	$(".click_dentaku").on('click',function(){
+	
+		//最初の入力をチェックする処理
+		//（+、*、/、0、00）が入力されたら変数を初期化して終了する
+		if(n1 == null && operator == null){
+			if($(this).val() == "+" || $(this).val() == "×" || $(this).val() == "÷" ){
+				operator = null;
+				n1 = null;
+				n2 = null;
+				minus = false;
+				operator_count = 0;
+				return;
+			}else{
+				if($(this).val() == "0" || $(this).val() == "00" ){
+					
+				  return;
+				}
+				//マイナスから始める時
+				if($(this).val() == "-"){
+					minus = true;
+					n1 = "-";
+				}
+			}
+		}
+	
+	
+		//数値が入力された時の処理
+		if($(this).attr('data-role') != 'den_operator'){
+			//最初がマイナス値の時はマイナスを数値につけてn1に格納する
+			if(minus){
+				n1 = n1 + $(this).val();
+				$(".dipl").val($(".dipl").val()+$(this).val());
+				operator_count = 0;
+				minus = false;
+				return;
+			}
+			
+			//入力された数値を変数n1、n2に格納し、電卓に表示する
+			if(n1 == null && operator == null){
+				$(".dipl").val('');			
+				$(".dipl").val($(".dipl").val()+$(this).val());
+				operator_count = 0;			
+				n1 = $(this).val();
+			
+			
+			}else if(n1 != null && operator == null){			
+				$(".dipl").val($(".dipl").val()+$(this).val());
+				operator_count = 0;
+				n1 = n1 + $(this).val();
+			
+			
+			}else if(n2 == null && operator != null){
+				
+			    $(".dipl").val($(".dipl").val()+$(this).val());
+				  operator_count = 0;	
+			    n2 = $(this).val();
+			    	
+			}else if(n2 != null && operator != null){
+				$(".dipl").val($(".dipl").val()+$(this).val());
+				operator_count = 0;
+				n2 = n2 + $(this).val();
+			}else{
+				return;
+			}
+
+		
+		//演算子が入力された時の処理
+		}else{
+			operator_count ++;
+			if(operator_count == 2 ){
+				operator_count = 1;
+			      
+				 
+			}else if(minus){
+				$(".dipl").val('');
+				$(".dipl").val($(".dipl").val()+$(this).val());
+			}else if(operator_count == 1 && n1 != null && n2 == null) {
+				operator = $(this).val();
+				$(".dipl").val($(".dipl").val()+$(this).val());
+				
+			
+			}else if(operator_count == 1 && n1 != null && n2 != null){
+			
+				//	二項演算子をcalculate()で計算する
+				calculate();
+				if(operator = $(this).val()){
+					   $(".dipl").val('');
+		         $(".dipl").val(n1);
+		         n2 = null;
+		         operator = $(this).val();
+		         $(".dipl").val($(".dipl").val()+$(this).val());
+		         operator_count = 0;
+				}
+				else{
+					return;
+				}
+				
+			}else{
+				return;
+			}	
+		}
+	});
+	
+	
+	
+	//四則演算子で計算する関数
+	function calculate(){
+		switch (operator){
+			case "÷":
+				n1 = Number(n1) / Number(n2);
+				break;
+			case "×":
+				n1 = Number(n1) * Number(n2);
+				break;
+			case "-":
+				n1 = Number(n1) - Number(n2);
+				break;	
+			case "+":
+				n1 = Number(n1) + Number(n2);
+				break;
+			default:
+				break;
+		 }
+    }
+	
+	
+	//イコールキー
+	$(".equal_b").on('click',function(){
+		calculate();
+		$(".dipl").val('');
+		$(".dipl").val(n1);
+		operator = null;
+		n2 = null;
+	    minus = false;
+		operator_count = 0;
+	});	
+	
+	
+	//クリアキー
+	$(".clear_b").on('click',function(){
+		$(".dipl").val('');
+		operator = null;
+		n1 = null;
+		n2 = null;
+		minus = false;
+		operator_count = 0;
+	});
+	
+	
+	
+});
